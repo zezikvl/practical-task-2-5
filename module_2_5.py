679758b3afed44a5ab13a1eb1a402bdd0073e869
zezikvl <zezik.vl@gmail.com>
2024-09-18 21:14:33 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -12,6 +12,8 @@ def get_matrix(n, m, value):
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
+result4 = get_matrix(4, 0, 13) # так как передал аргумент со значением 0, то возвращается пустой список
 print(result1)
 print(result2)
 print(result3)
+print(result4)
